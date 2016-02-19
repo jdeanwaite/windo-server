4ab0a257b5cc81f30fcfb0a0da6d490601f71e4e
Mohonri Dorff <Mohonri@Mohonris-MacBook-Pro.local>
2016-02-18 22:42:38 -0700

    test

---
+++ public/app/login/login.component.js
@@ -0,0 +1,13 @@
+angular.module('windoApp').directive('login', function () {
+  return {
+    restrict: 'E',
+    templateUrl: '/app/templates/dashboard',
+    controllerAs: 'dashboard',
+    controller: function ($http) {
+
+
+
+
+    }
+  }
+});
